f7ff14cdd2317b1446abbe934888653794607f56
VerdantVibes <VerdantVibes@github.com>
2024-10-03 02:47:25 +0200

    feat: add run

--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -8,6 +8,7 @@ from sqlmodel import SQLModel
 from alembic import context
 from app.config import get_settings
 from app.database.agent import *
+
 # import models
 
 # get app settings
@@ -57,44 +58,3 @@ def run_migrations_offline() -> None:
 
     with context.begin_transaction():
         context.run_migrations()
-
-
-def do_run_migrations(connection: Connection) -> None:
-    context.configure(connection=connection, target_metadata=target_metadata)
-
-    with context.begin_transaction():
-        context.run_migrations()
-
-
-async def run_async_migrations() -> None:
-    """In this scenario we need to create an Engine
-    and associate a connection with the context.
-
-    """
-    cfg = config.get_section(config.config_ini_section, {})
-
-    # change sqlalchemy.url as env value
-    cfg["sqlalchemy.url"] = app_settings.PG_AGENT_DATABASE_URL
-
-    connectable = async_engine_from_config(
-        cfg,
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
-
-    async with connectable.connect() as connection:
-        await connection.run_sync(do_run_migrations)
-
-    await connectable.dispose()
-
-
-def run_migrations_online() -> None:
-    """Run migrations in 'online' mode."""
-
-    asyncio.run(run_async_migrations())
-
-
-if context.is_offline_mode():
-    run_migrations_offline()
-else:
-    run_migrations_online()
